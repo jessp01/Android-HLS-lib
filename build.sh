956340d4936da87c10ef1288df1183df6ede4cf7
Jess Portnoy <jess.portnoy@kaltura.com>
2015-06-17 13:41:55 +0300

    some debug prints.

--- build.sh
+++ build.sh
@@ -31,4 +31,6 @@ export NDK_PROJECT_PATH=`pwd`/HLSPlayerSDK/
 sed -i "s@\${aapt}@${ANDROID_HOME}/tools/aapt@g" `pwd`/android-sdk-linux/tools/ant/build.xml
 sed -i s#@NDK_BUILD_PATH@#`pwd`/android-ndk-$NDK_VER/ndk-build#g build.xml
 ln -sf $ANDROID_HOME/build-tools/23.0.0_rc2/aapt ${ANDROID_HOME}/tools
+ls -al ${ANDROID_HOME}/tools/aapt $ANDROID_HOME/build-tools/23.0.0_rc2/aapt
+ldd ${ANDROID_HOME}/tools/tools/aapt
 cd HLSPlayerSDK && ndk-build && ant release
